6029a3dd934481060c8a4a4dc4c7e068d14f6e63
Sayeeswarnan S <116819369+sayeeswarnan@users.noreply.github.com>
2024-03-01 19:21:53 +0530

    Add files via upload

---
+++ objects/object.js
@@ -0,0 +1,13 @@
+//objects - A Collection of related properties and or methods Can represent real 
+// world objects 
+//object={key:value,function()}
+
+// object cannot have same name
+
+const person ={
+    first:"sai",
+    lastname:"swaran",
+    age:20,
+    employed:true,
+}
+console.log(person.first);
